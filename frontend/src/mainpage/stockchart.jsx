36fb3226469f3bfeab49358129814ba1cb2f51a1
Tanay singh <tanaysingh906@gmail.com>
2026-06-27 15:53:13 +0530

    Update stockchart.jsx

--- frontend/src/mainpage/stockchart.jsx
+++ frontend/src/mainpage/stockchart.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { Line } from "react-chartjs-2";
 import "chart.js/auto";
-import "./stockChart.css";
+import "./stockchart.css";
 
 function StockChart({ symbol }) {
   const [chartData, setChartData] = useState([]);

9c31b25da6e342d7dfb06f17dfa97ce8e323009c
Tanay singh <tanaysingh906@gmail.com>
2026-06-27 17:42:06 +0530

    Update news.js

--- backend/controller/news.js
+++ backend/controller/news.js
@@ -1,16 +1,16 @@
-import axios from "axios";
-
 export const getNews = async (req, res) => {
   try {
     const { id } = req.params;
 
-    const response = await axios.get(
+    const response = await fetch(
       `https://gnews.io/api/v4/search?q=${id}&lang=en&max=5&token=${process.env.GNEWS_API_KEY}`
     );
 
-    res.status(200).json(response.data);
+    const data = await response.json();
+
+    res.status(200).json(data);
   } catch (error) {
-    console.error(error.response?.data || error.message);
+    console.error(error);
 
     res.status(500).json({
       message: "Failed to fetch news",

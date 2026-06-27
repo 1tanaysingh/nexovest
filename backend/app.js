d4e2d0b08d1a9286e33913628b71dc544b24f4ec
Tanay singh <tanaysingh906@gmail.com>
2026-06-27 16:24:35 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -15,10 +15,12 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      // "http://localhost:5173",
+      "https://nexovest-frontend3.onrender.com",
+    ],
     credentials: true,
   }),
 );
